cfced947444334fd057500bf6bea71cc92dcf615
paulinavo22 <paulinavasquezo2211@gmail.com>
2025-09-18 17:37:34 -0600

    Add circle

--- paint.py
+++ paint.py
@@ -10,7 +10,7 @@ Exercises
 """
 
 from turtle import *
-
+import turtle
 from freegames import vector
 
 
@@ -38,8 +38,15 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y - ((end.x - start.x)**2 + (end.y - start.y)**2) ** 0.5)  
+    down()
+    begin_fill()
 
+    radius = ((end.x - start.x)**2 + (end.y - start.y)**2) ** 0.5
+    turtle.circle(radius)
+
+    end_fill()
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
